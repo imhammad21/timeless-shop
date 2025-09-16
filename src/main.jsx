038d682f1f4fc9c2b7f924635ef792ddfd388e28
imhammad21 <arainhammad159@gmail.com>
2025-09-16 20:10:45 +0500

    Updated project with new changes

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import "./style.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/timeless-shop">
       <CartProvider>
         <App />
       </CartProvider>
