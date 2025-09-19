c53a726b996983c59e1ceb624153063322b68bad
imhammad21 <arainhammad159@gmail.com>
2025-09-19 17:00:36 +0500

    Your update message

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "/timeless-shop/",   // 👈 must have leading & trailing slash
+  build:{outDir:'dist'},
   resolve: {
     alias: {
       '@': '/src',
